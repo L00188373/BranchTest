187c305002836cd4c28560c697d35fa9db315205
Afjm72 <L00188373@atu.ie>
2024-10-19 14:14:56 +0100

    first commit

---
+++ setup.cmd
@@ -0,0 +1,8 @@
+echo "# BranchTest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/L00188373/BranchTest
+
+git push -u origin main
